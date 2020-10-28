30ab4e0c28583dfc6c7879bed9714570115ea00d
SamantaMelissa <samanta.melissa12@gmail.com>
2020-10-28 18:26:16 -0200

    aula 7

---
+++ login/src/components/Rodape/index.jsx
@@ -0,0 +1,14 @@
+//using EntityFrameworkCore
+import React from 'react';
+import './Rodape.css'
+
+//Renderiza
+function Rodape(){
+    return(
+        <footer className='footer'>
+            <h1>Senai Informática 132</h1>
+        </footer>
+    )
+}
+//Deixa vísivel para outros
+export default Rodape;
